7f893def79cefdc0872936e583a46502291434ee
Jjuhui313 <hyacinth92928@gmail.com>
2023-03-05 03:42:42 +0900

    test: kurlyObackrepo에 있는 내용 옮김

--- kurlyO/src/main/java/com/sparta/kurlyo/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.kurlyo.repository;
-
-
-import com.sparta.kurlyo.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-    Category findByName(String name);
-}
--- kurlyO/src/main/java/com/sparta/kurlyo/repository/GoodsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.kurlyo.repository;
-
-import com.sparta.kurlyo.entity.Goods;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GoodsRepository extends JpaRepository<Goods, Long> {
-}
