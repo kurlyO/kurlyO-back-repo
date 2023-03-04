6f7506f2156a1fc6a120dc3cee1621f1db5b71a8
Jjuhui313 <hyacinth92928@gmail.com>
2023-03-05 03:45:45 +0900

    test: kurlyObackrepo에 있는 내용 옮김

---
+++ src/main/java/com/sparta/kurlyo/repository/MembersRepository.java
@@ -0,0 +1,14 @@
+package com.sparta.kurlyo.repository;
+
+import com.sparta.kurlyo.entity.Members;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MembersRepository extends JpaRepository<Members, Long> {
+    Optional<Members> findByAccount(String account);
+    Optional<Members> findByEmail(String email);
+
+    Optional<Members> findByMemberName(String name);
+
+}
